8c4c02529447751c0099f362bb48d1852ca495cd
Guy Luong <16323642+firisthetraveller@users.noreply.github.com>
2026-02-06 15:31:31 +0100

    [Fix] Deploy script on Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from 'next'
- 
+
+const repoName = "interactive-cv-firebase";
+
 const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
+  
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 }
- 
+
 export default nextConfig;
