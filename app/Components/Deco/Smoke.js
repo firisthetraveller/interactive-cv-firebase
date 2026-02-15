d8abb7fec0ac9bc694199657bc126cad9ac903f1
Guy Luong <16323642+firisthetraveller@users.noreply.github.com>
2026-02-15 02:46:31 +0100

    [Clean] Refactor position
    Doesn't fix smoke

--- app/Components/Deco/Smoke.js
+++ app/Components/Deco/Smoke.js
@@ -5,14 +5,16 @@ const clamp = (v, min, max) => {
 }
 
 const Smoke = () => {
+    const [sourceX, sourceY] = [650, 528];
+
     return (
         <>
             {
                 Array.from({ length: 5 }, (e, i) => i).map(i =>
                     <motion.circle
                         key={i}
-                        initial={{ cx: 650, cy: 528, r: 3, fillOpacity: 1 }}
-                        animate={{ cx: 650 + clamp(Math.random() * 100, 50, 150), cy: 354 - clamp(Math.random() * 340, 100, 170), r: 15, fillOpacity: 0 }}
+                        initial={{ cx: sourceX, cy: sourceY, r: 3, fillOpacity: 1 }}
+                        animate={{ cx: sourceX + clamp(Math.random() * 100, 50, 150), cy: sourceY * 2 / 3 - clamp(Math.random() * 340, 100, 170), r: 15, fillOpacity: 0 }}
                         transition={{ duration: 3 + clamp(Math.floor(Math.random() * 5), 2, 4), repeat: Infinity, ease: "easeOut", delay: Math.random() }}
                         fill="gray"
                     />
