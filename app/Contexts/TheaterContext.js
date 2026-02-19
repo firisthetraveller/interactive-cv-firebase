0fb55ae386f2cddbd91de283c4fff8c6e9d48a4d
Guy Luong <16323642+firisthetraveller@users.noreply.github.com>
2026-02-19 11:21:48 +0100

    [Update] Docs

--- app/Contexts/TheaterContext.js
+++ app/Contexts/TheaterContext.js
@@ -5,11 +5,19 @@ export const TheaterContext = createContext(null);
 export const TheaterProvider = ({children}) => {
     const [state, dispatch] = useReducer(theaterReducer, null);
 
+    /**
+     * Switch the current theater image to the one linked in the URL.
+     * @param {string} url 
+     * @param {string?} color 
+     */
     const switchMovie = (url, color = null) => {
         // console.log(`Called switch: ${url} ${color}`)
         dispatch({type: "switch", url, color});
     }
 
+    /**
+     * Remove the set image in the theater.
+     */
     const clearTheater = () => {
         // console.log("Called clear.")
         dispatch({type: "clear"});
