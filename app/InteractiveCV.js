ed640d03a932f93550fbd371d9aa4791e7b5ea33
Guy Luong <16323642+firisthetraveller@users.noreply.github.com>
2026-03-05 14:16:12 +0100

    [Fix] Removed LoadContext

--- app/InteractiveCV.js
+++ app/InteractiveCV.js
@@ -1,9 +1,19 @@
 import { LightProvider } from "./Contexts/LightContext";
 import { TheaterProvider } from "./Contexts/TheaterContext";
-import { LoadProvider } from "./Contexts/LoadContext";
+
 import Highlights from "./Components/Highlights";
 import SideView from "./Components/SideView";
 import StarrySky from "./Components/Deco/StarrySky";
+
+
+const Content = () => {
+    return (
+        <div className="xl:flex relative xl:h-full z-10">
+            <SideView />
+            <Highlights />
+        </div>
+    )
+}
 
 const InteractiveCV = () => {
     return (
@@ -15,12 +25,7 @@ const InteractiveCV = () => {
             <div className="px-8 2xl:px-16 pt-8 bg-linear-to-b from-slate-900 from-50% via-teal-700 via-85% to-cyan-400">
                 <LightProvider>
                     <TheaterProvider>
-                        <LoadProvider>
-                            <div className="xl:flex relative xl:h-full z-10">
-                                <SideView />
-                                <Highlights />
-                            </div>
-                        </LoadProvider>
+                        <Content />
                     </TheaterProvider>
                 </LightProvider>
             </div>
