e596e67bf3b472b029316e6bf2a0cd0a3d07300f
Guy Luong <16323642+firisthetraveller@users.noreply.github.com>
2026-03-13 01:34:48 +0100

    [Responsive] Fix guardrail on higher resolution

--- app/Components/Base/HoverableTag.js
+++ app/Components/Base/HoverableTag.js
@@ -1,6 +1,7 @@
 import { createRef, useEffect, useState } from "react";
 import useLights from "../../Hooks/useLights";
 import ResizeSVG from "./ResizeSVG";
+import useWindowProperties from "../../Hooks/useWindowProperties";
 
 /**
  * 
@@ -10,27 +11,36 @@ import ResizeSVG from "./ResizeSVG";
  */
 const HoverableTag = ({ name }) => {
     const { toggleLight, isActiveLight, isPermanentLight } = useLights();
+    const { width } = useWindowProperties();
     const ref = createRef();
     const [divWidth, setDivWidth] = useState(null);
+    const step = (Math.min(width, 4650) / 360) | 0;
+    const height = step * 2;
 
     useEffect(() => {
         if (!divWidth && ref.current) {
-            setDivWidth(Math.floor(ref.current.getBoundingClientRect().width));
+            setDivWidth(Math.floor(ref.current.getBoundingClientRect().width + 8));
         }
     }, [ref]);
 
+    useEffect(() => {
+        if (ref.current) {
+            setDivWidth(Math.floor(ref.current.getBoundingClientRect().width + 8));
+        }
+    }, [width]);
+
     return (
-        <div ref={ref} className="relative">
-            <ResizeSVG width={divWidth} height={8} minWidth={1280} className="pointer-events-none bottom-0 absolute">
+        <div className="relative">
+            <ResizeSVG width={divWidth} height={height} minWidth={1280} className="pointer-events-none bottom-0 absolute">
                 {/** TODO: Curtains */}
 
                 {/** This is the guardrail */}
                 <line x1={0} y1={0} x2={divWidth} y2={0} strokeWidth={1} stroke="#475569" />
-                {Array.from({ length: divWidth / 4 }, (e, i) => i + 1).map(i => <line key={i} x1={4 + 4 * i} y1={0} x2={4 + 4 * i} y2={8} strokeWidth={1} strokeOpacity={0.4} stroke="#475569" />)}
+                {Array.from({ length: (divWidth / step)|0 }, (e, i) => i + 1).map(i => <line key={i} x1={step + step * i} y1={0} x2={step + step * i} y2={height} strokeWidth={(step / 4) | 0} strokeOpacity={0.4} stroke="#475569" />)}
             </ResizeSVG>
 
             {/** The window lighting up and turning off */}
-            <div className={`
+            <div ref={ref} className={`
                     my-0.5 mx-1 p-1 cursor-pointer
                     ${isPermanentLight(name)
                     ? "bg-teal-100 shadow-lg shadow-teal-200 text-slate-800"
